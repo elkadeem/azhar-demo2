37f9e57143c82f33cced348ec83e316a807f821f
Wael Elkadim <elkadeem@hotmail.com>
2022-05-18 12:08:34 +0200

    Add address

--- azhar-demo/Customer.cs
+++ azhar-demo/Customer.cs
@@ -10,6 +10,8 @@ namespace azhar_demo
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }    
+        public string Name { get; set; }
+
+        public string Address { get; set; }
     }
 }
